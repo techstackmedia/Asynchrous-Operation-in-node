33354b731760e69680d95a3aa8a230af055cb4ba
techstackmedia <osagienoah04@gmail.com>
2022-01-08 19:23:47 +0100

    remove comments

--- 01-callbackHell/app.js
+++ 01-callbackHell/app.js
@@ -1,27 +1,23 @@
 const getUser = (id, callback) => {
   setTimeout(() => {
-    // console.log(id);
     callback({ id: id, gitHubRepo: "techstackmedia" });
   }, 1000);
 };
 
 const getRepository = (username, callback) => {
   setTimeout(() => {
-    // console.log(username);
     callback(["repo1", "repo2", "repo3"]);
   }, 1000);
 };
 
 const getCommit = (commit, callback) => {
   setTimeout(() => {
-    // console.log(commit);
     callback(["First commit", "Last commit"]);
   }, 1000);
 };
 
 const callbackHell = (comment, callback) => {
   setTimeout(() => {
-    // console.log(comment);
     callback(["This is a callback hell", "This is a christmas tree problem"]);
   }, 1000);
 };
