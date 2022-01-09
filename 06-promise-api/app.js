afcaadb69ccce8923d008e970680c175c1150018
techstackmedia <osagienoah04@gmail.com>
2022-01-09 13:55:11 +0100

    add a promise-api folder

--- 06-promise-api/app.js
+++ 06-promise-api/app.js
@@ -1,13 +1,24 @@
-const getUser = (id) => {
-  setTimeout(() => {
-    console.log("Kick off an asyn operation...");
+const isGithubRepository = false;
 
-    const resolvedPromise = Promise.resolve({
-      id: id,
-      gitHubRepository: "techstackmedia",
-    });
-    resolvedPromise.then((result) => console.log(result));
-  }, 1000);
+const getUser = (id) => {
+  if (isGithubRepository) {
+    setTimeout(() => {
+      console.log("Kick off an async operation...");
+
+      const resolvedPromise = Promise.resolve({
+        id: id,
+        gitHubRepository: "techstackmedia",
+      });
+      resolvedPromise.then((result) => console.log(result));
+    }, 1000);
+  } else {
+    setTimeout(() => {
+      console.log("Kick off an async operation...");
+
+      const rejectedPromise = Promise.reject(new Error("Error Message"));
+      rejectedPromise.catch((err) => console.error(err.message));
+    }, 1000);
+  }
 };
 
 getUser(1);
